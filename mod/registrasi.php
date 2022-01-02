ea2902814b2ec74590d69937251433ff8de24aec
kydo28 <rizkywidodo2801@gmail.com>
2022-01-02 17:46:30 +0700

    tampilan

--- mod/registrasi.php
+++ mod/registrasi.php
@@ -17,10 +17,8 @@ $kode_karyawan = $huruf . sprintf("%03s", $urutan);
  echo'
  <!-- App Capsule -->
     <div id="appCapsule">
-        <div style="background:#00B4FF;border-radius:30px;margin:0 16px;padding:10px 15px" class="section text-center">
-            <h1 style="color:#FFFFFF;font-size:24px;"><i class="fa fa-user-plus"></i> Register</h1>
-            <img src="'.$site_url.'/content/'.$site_logo.'" height="70">
-            <h4 style="color:#FFFFFF;">Silakan mengisi data pendaftaran akun dengan valid</h4>
+        <div class="section text-center">
+            <h1 class="text-primary" style="font-size:24px;"><i class="text-primary fa fa-user-plus"></i><br>Register</h1>
         </div>
         <div class="section mb-5 p-2">
             <form id="form-registrasi">
@@ -96,33 +94,34 @@ $kode_karyawan = $huruf . sprintf("%03s", $urutan);
                         </div>
 
                         <div class="form-group basic">
-                            <div class="input-wrapper">
-                                <label class="label" for="password1">Password</label>
-                                <input type="password" class="form-control" id="password" name="employees_password"  placeholder="Masukkan Password" required>
-                                <i class="clear-input"><ion-icon name="close-circle"></ion-icon></i>
-                            </div>
+                        <div class="input-wrapper">
+                            <label class="label" for="password1">Password</label>
+                            <input type="password" class="form-control" id="password" name="employees_password"  placeholder="Masukkan password" required>
                         </div>
                     </div>
                 </div>
-                <div class="form-links mt-2">
-                    <div>
-                        <a class="btn btn-success" href="login"><i class="fa fa-user"></i> Login</a>
-                    </div>
-                    <div>
-                        <a class="btn btn-danger" href="forgot"><i class="fa fa-key"></i> Reset Password</a>
-                    </div>
+            </div>
+
+            <div class="p-2">
+                <div class="pb-1">
+                    <button type="submit" class="btn btn-primary btn-block"></i>Daftar</button>
                 </div>
-
-                <div class="form-button-group transparent">
-                   <button type="submit" class="btn btn-primary btn-block"><i class="fa fa-user-plus"></i> Register</button>
-                   <a href="oauth/google" class="btn btn-warning btn-block"><ion-icon name="logo-google"></ion-icon> Register with Google</a>
+                    <p class="text-center text-secondary">atau</p>
+                <div class="pb-4">
+                    <a href="oauth/google" class="btn btn-light btn-block">
+                    <svg class="mr-1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24" height="24" viewBox="0 0 48 48"><path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"/><path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"/><path fill="#4CAF50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"/><path fill="#1976D2" d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"/></svg>
+                    Daftar dengan Google</a>
                 </div>
+                <div class="pb-4 text-center">
+                    <p>Sudah memiliki akun?<a href="login" class="text-primary"> Login</a></p>
+                </div>
+            </div>
 
-            </form>
-        </div>
-
+        </form>
     </div>
-    <!-- * App Capsule -->';}
+
+</div>
+<!-- * App Capsule -->';}
   else{
   }
 
